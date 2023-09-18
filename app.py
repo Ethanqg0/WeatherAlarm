44e41683973d252fb58256b65158b64a0b5d44f5
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2023-09-18 14:26:05 -0700

    automation for clients

--- app.py
+++ app.py
@@ -3,7 +3,16 @@ import time
 import requests 
 import re
 from datetime import datetime
-from twilio.rest import Client
+from twilio.rest import Client as TwilioClient
+from supabase import create_client, Client
+
+url = "https://wonclqrwdjpiodpgwbgs.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvbmNscXJ3ZGpwaW9kcGd3YmdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ1NzI0OTAsImV4cCI6MjAxMDE0ODQ5MH0.fMJHVn5ECaWJA7pn18oPreKTCwtBS7g32N_M4NH55A8"
+Client = create_client(url, key)
+
+def get_clients():
+    clients = Client.table("clients").select("*").execute().data
+    return clients
 
 def get_weather(latitude, longitude):
     base_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m&daily=sunrise,sunset&timezone=America%2FLos_Angeles"
@@ -11,55 +20,41 @@ def get_weather(latitude, longitude):
     data = response.json()
     return data
 
-client_numbers = ["7089667431", "9254512502", "4159374283", "6307886898"]
-
-def send_text_message(body):
+def send_text_message(body, phone):
     account_sid = "ACdfe74ca16e90ea5ecd2da2e5d9dd7321"
     auth_token = "9e43cec869d648e2a59811ced3172f67"
     from_phone_number = "+18333061864"
-    to_phone_number = "7089667431"
+    to_phone_number = phone
 
-    client = Client(account_sid, auth_token)
+    twilio_client = TwilioClient(account_sid, auth_token)
 
-    message = client.messages.create(
+    message = twilio_client.messages.create(
         body=body,
         from_=from_phone_number,
         to=to_phone_number
     )
     print("Text message sent! :)")
 
-def send_weather_update():
-    latitude = 37.9891
-    longitude = -122.0436
 
-    weather_data = get_weather(latitude, longitude)
+def send_weather_update(clients):
+    for client in clients:
+        weather_data = get_weather(client['latitude'], client['longitude'])
 
-    temperature_celsius = weather_data["hourly"]["temperature_2m"][8]
-    temperature_fahrenheit = round((temperature_celsius * 9/5) + 32)
+        temperature_celsius = weather_data["hourly"]["temperature_2m"][13]
+        temperature_fahrenheit = round((temperature_celsius * 9/5) + 32)
 
-    sunrise = weather_data["daily"]["sunrise"][0].split('T')[1]
-    sunset = weather_data["daily"]["sunset"][0].split('T')[1]
+        sunrise = weather_data["daily"]["sunrise"][0].split('T')[1]
+        sunset = weather_data["daily"]["sunset"][0].split('T')[1]
 
-    weather_info = {
-        f"Good morning from Firewave! :)\n"
-        f"Current Weather in Concord, CA:\n"
-        f"Temperature: {temperature_fahrenheit}°F\n"
-        f"Sunrise: {sunrise}\n"
-        f"Sunset: {sunset}\n"
-        f"Have a great day!\n\n\n"
-    }
+        weather_info = {
+            f"Good morning from Firewave! :)\n"
+            f"Today's Weather in {client['city']}:\n"
+            f"Current Temperature: {temperature_fahrenheit}°F\n"
+            f"Sunrise: {sunrise}\n"
+            f"Sunset: {sunset}\n"
+            f"Have a great day, {client['name']}!\n\n\n"
+        }
 
-    send_text_message(weather_info)
+        send_text_message(weather_info, client['number'])
 
-send_weather_update()
-
-"""
-def main():
-    schedule.every().day.at("08:00").do(send_weather_update)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-if __name__ == "__main__":
-    main()
-"""
+send_weather_update(get_clients())
---
+++ authentication.py
@@ -0,0 +1,8 @@
+import requests
+import openai
+import os
+from supabase import create_client, Client
+url = "https://wonclqrwdjpiodpgwbgs.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvbmNscXJ3ZGpwaW9kcGd3YmdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ1NzI0OTAsImV4cCI6MjAxMDE0ODQ5MH0.fMJHVn5ECaWJA7pn18oPreKTCwtBS7g32N_M4NH55A8"
+Client = create_client(url, key)
+
