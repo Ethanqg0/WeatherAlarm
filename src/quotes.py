05c2692f908b57d0c3bd5350b756e206e2efd962
Samin Amanat <amanat361@gmail.com>
2023-09-27 22:52:53 -0700

    added docstrings and type annotations to all funcs

--- src/quotes.py
+++ src/quotes.py
@@ -1,8 +1,14 @@
 import requests
 
-def get_quote():
+def get_quote() -> str:
+    """
+    Retrieves a random quote from the Quotable API.
+
+    Returns:
+        str: A string containing the quote and author.
+    """
     api_url = "https://api.quotable.io/quotes/random"
     response = requests.get(api_url)
     data = response.json()
-    quote = data[0]["content"] + " - " + data[0]["author"]
+    quote = data["content"] + " - " + data["author"]
     return quote
--- src/twilio.py
+++ src/twilio.py
@@ -4,7 +4,17 @@ from twilio.rest import Client as TwilioClient
 
 dotenv.load_dotenv()
 
-def send_text_message(body, phone):
+def send_text_message(body: str, phone: str) -> None:
+    """
+    Sends a text message using Twilio's API.
+
+    Args:
+        body (str): The body of the text message.
+        phone (str): The phone number to send the text message to.
+
+    Returns:
+        None
+    """
     account_sid = os.getenv("TWILIO_SID")
     auth_token = os.getenv("TWILIO_AUTH_TOKEN")
     from_phone_number = "+18333061864"
