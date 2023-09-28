dda0de04ade67ee1dc470c92aea19d49b0fea6e6
Samin Amanat <amanat361@gmail.com>
2023-09-27 22:59:22 -0700

    Reverted change in quotes file where index is not accessed correctly

--- src/quotes.py
+++ src/quotes.py
@@ -10,5 +10,5 @@ def get_quote() -> str:
     api_url = "https://api.quotable.io/quotes/random"
     response = requests.get(api_url)
     data = response.json()
-    quote = data["content"] + " - " + data["author"]
+    quote = data[0]["content"] + " - " + data[0]["author"]
     return quote
