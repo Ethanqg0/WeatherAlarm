ec8482a58b1cc3bf776b6fad1b79df6b79885363
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2023-09-29 14:08:15 -0700

    fixing import bug

--- tests/test_weatherAPI.py
+++ tests/test_weatherAPI.py
@@ -1,4 +1,4 @@
-from weather_api import grade_aqi, get_clients, get_test_clients, get_weather, fetch_air_quality_data, find_max_aqi
+from src.weather_api import grade_aqi, get_clients, get_test_clients, get_weather, fetch_air_quality_data, find_max_aqi
 import pytest
 
 @pytest.mark.parametrize("input_aqi, expected_grade", [
