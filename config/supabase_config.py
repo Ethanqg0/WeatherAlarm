ccc972efd0bef5b3d00c54dfc09047f1f91ec4f3
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2023-09-28 22:05:38 -0700

    push

--- config/supabase_config.py
+++ config/supabase_config.py
@@ -5,6 +5,6 @@ import requests
 
 load_dotenv()
 
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
+url = "https://wonclqrwdjpiodpgwbgs.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvbmNscXJ3ZGpwaW9kcGd3YmdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ1NzI0OTAsImV4cCI6MjAxMDE0ODQ5MH0.fMJHVn5ECaWJA7pn18oPreKTCwtBS7g32N_M4NH55A8"
 Client = create_client(url, key)
