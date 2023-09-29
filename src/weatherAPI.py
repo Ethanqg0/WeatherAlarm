b625ce5a0be255e9cfe9e399061af400cc3d83c1
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2023-09-29 13:21:26 -0700

    adding module docstring, fixing whitespace, splitting long lines

--- src/weatherAPI.py
+++ src/weatherAPI.py
@@ -1,7 +1,38 @@
+"""
+This module provides functions for retrieving weather and air quality data, grading AQI values,
+and sending weather updates to clients.
+
+Modules imported:
+- `config.supabase_config`: Configuration for Supabase database.
+- `src.twilio`: Functions for sending text messages via Twilio.
+- `src.quotes`: Functions for retrieving inspirational quotes.
+- `requests`: HTTP library for making API requests.
+- `dotenv`: Loading environment variables from a .env file.
+
+Functions provided:
+- `get_clients() -> list`: Retrieves a list of clients from the Supabase database.
+
+- `get_test_clients()`: Retrieves test clients' data from the Supabase database.
+
+- `get_weather(latitude: float, longitude: float) -> dict`: Retrieves weather data
+  from the Open Meteo API for a given latitude and longitude.
+
+- `fetch_air_quality_data(latitude: float, longitude: float) -> dict`: Retrieves air quality data
+  from the Open Meteo API for a given latitude and longitude.
+
+- `find_max_aqi(data: dict) -> int`: Finds the maximum AQI value in the hourly US AQI data.
+
+- `grade_aqi(aqi: int) -> str`: Assigns a grade to an AQI value.
+
+- `send_weather_update(clients: list) -> None`: Sends a weather update to a list of clients.
+
+For more information on each function, refer to their respective docstrings.
+"""
+
 from config.supabase_config import *
 from src.twilio import *
 from src.quotes import *
-import requests 
+import requests
 import dotenv
 
 dotenv.load_dotenv()
@@ -16,8 +47,8 @@ def get_clients() -> list:
     return Client.table("clients").select("*").execute().data
 
 def get_test_clients():
-  return Client.table("test_clients").select("*").execute().data
-  
+    return Client.table("test_clients").select("*").execute().data
+
 def get_weather(latitude: float, longitude: float) -> dict:
     """
     Retrieves weather data from the Open Meteo API for a given latitude and longitude.
@@ -29,7 +60,13 @@ def get_weather(latitude: float, longitude: float) -> dict:
     Returns:
         dict: A dictionary containing weather data for the specified location.
     """
-    base_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m&daily=sunrise,sunset&timezone=America%2FLos_Angeles"
+    base_url = (
+    f"https://api.open-meteo.com/v1/forecast?"
+    f"latitude={latitude}&longitude={longitude}"
+    f"&hourly=temperature_2m&daily=sunrise,sunset"
+    f"&timezone=America%2FLos_Angeles"
+    )
+
     response = requests.get(base_url)
     data = response.json()
     return data
@@ -45,7 +82,11 @@ def fetch_air_quality_data(latitude: float, longitude: float) -> dict:
     Returns:
         dict: A dictionary containing air quality data for the specified location.
     """
-    base_url = f"https://air-quality-api.open-meteo.com/v1/air-quality?latitude={latitude}&longitude={longitude}&hourly=us_aqi&timezone=America%2FLos_Angeles"
+    base_url = (
+    f"https://air-quality-api.open-meteo.com/v1/air-quality?"
+    f"latitude={latitude}&longitude={longitude}"
+    f"&hourly=us_aqi&timezone=America%2FLos_Angeles"
+    )
     response = requests.get(base_url)
     data = response.json()
     return data
@@ -62,7 +103,7 @@ def find_max_aqi(data: dict) -> int:
     """
     us_aqi = data["hourly"]["us_aqi"]
     max_aqi = us_aqi[0]
-    for aqi in us_aqi: 
+    for aqi in us_aqi:
         if aqi is None:
             continue
         if aqi > max_aqi:
@@ -119,7 +160,7 @@ def send_weather_update(clients: list) -> None:
         aqi = str(aqi) + grade_aqi(aqi)
 
         quote = get_quote(client['category'])
-            
+
         weather_info = {
             f"Good morning! :)\n\n"
             f"Today's Weather in {client['city']}:\n"
