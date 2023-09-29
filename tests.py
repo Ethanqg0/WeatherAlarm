079128933471760737af145aef7ded7560796f95
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2023-09-28 22:09:26 -0700

    fix

--- tests.py
+++ tests.py
@@ -13,24 +13,24 @@ supabase = create_client(url, key)
 
 def test_grade_aqi_good():
     result = grade_aqi(30)
-    assert result == "Good - Great!"
+    assert result == "- Great!"
 
 def test_grade_aqi_moderate():
     result = grade_aqi(75)
-    assert result == "Moderate!"
+    assert result == "- Moderate!"
 
 def test_grade_aqi_unhealthy_sensitive_groups():
     result = grade_aqi(120)
-    assert result == "Unhealthy for Sensitive Groups!"
+    assert result == "- Unhealthy for Sensitive Groups!"
 
 def test_grade_aqi_unhealthy():
     result = grade_aqi(180)
-    assert result == "Unhealthy!"
+    assert result == "- Unhealthy!"
 
 def test_grade_aqi_very_unhealthy():
     result = grade_aqi(250)
-    assert result == "Very Unhealthy!"
+    assert result == "- Very Unhealthy!"
 
 def test_grade_aqi_hazardous():
     result = grade_aqi(350)
-    assert result == "Hazardous!"
+    assert result == "- Hazardous!"
